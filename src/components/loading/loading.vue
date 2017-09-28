c767fd3641b33a66d01157befa84a4d145382375
manlili <manlili@lechebang.com>
2017-09-28 14:45:19 +0800

    优化加载中组件

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="loading-box">
-        Loading...
+    <div class="loading-wrap">
+    	<h4>组件一:loading组件展示</h4>
+        <div class="loading-cont">
+        	Loading...
+        </div>
     </div>
 </template>
 <script></script>
@@ -9,4 +12,13 @@
 </script>
 
 <style>
+	.loading-cont {
+		width: 50%;
+		background-color: rgba(0, 0, 0, .5);
+		border-radius: 5px;
+		padding: 5px;
+		text-align: center;
+		color: #fff;
+		margin: 0 auto;
+	}
 </style>
