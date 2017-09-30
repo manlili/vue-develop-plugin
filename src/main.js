04cbedb3d24d335e8242dab24f490bb9bb4e1158
manlili <manlili@lechebang.com>
2017-09-30 16:15:57 +0800

    change file name

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 
 //使用方法3创建sendUbt组件
-import sendUbt from './components/sendUbt'
+import sendUbt from './plugins/sendUbt'
 Vue.use (sendUbt)
 
 //使用方法1创建alert插件
-//import alert from './components/alert'
+//import alert from './plugins/alert'
 //Vue.use(alert)
 ////注意这里是alert使用的方式,因为alert是Vue全局方法
 //Vue.alert()   
 
 //使用方法4创建doubleNumber 插件
-//import doubleNumber from './components/doubleNumber'
+//import doubleNumber from './plugins/doubleNumber'
 //Vue.use(doubleNumber)
 
 //使用方法5创建loading 插件
-//import Loading from './components/loading'
+//import Loading from './plugins/loading'
 //Vue.use(Loading)
 
 new Vue({
